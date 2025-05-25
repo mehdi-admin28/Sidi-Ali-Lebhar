f074d1f6ce0b88c6b880b82492cfcb494a9a6b4f
mehdi-admin28 <mehdi.chek6514@gmail.com>
2025-05-25 15:15:43 +0100

    perfectionner les détaille

--- prof.js
+++ prof.js
@@ -3,7 +3,9 @@ let li012 = document.querySelector('li.li01');
 let navbar = document.querySelector('.navbar');
 let dispare = document.querySelector('.dispare');
 let navbar1 = document.querySelector('.navbar1');
-let opt = document.querySelector('.opt');
+let opt = document.querySelectorAll('.opt');
+let h101 = document.querySelector('.h101');
+
 
 menu.addEventListener('click',()=>{
     li012.classList.toggle('dis');
@@ -22,9 +24,15 @@ window.addEventListener('scroll',()=>{
     if(window.scrollY<100){
         dispare.style.top="40%";
         navbar1.style.background="transparent";
+        opt.forEach(x=>{x.style.fontSize="1.4rem";});
+        h101.style.fontSize="1.7rem";
     }
     else{dispare.style.top="-11rem";
         navbar1.style.background="#2C3E50";
+        opt.forEach(x=>{
+            x.style.fontSize="1rem";
+        });
+        h101.style.fontSize="1.2rem";
     }
 })
 document.addEventListener("submit",(e)=>{
